69552c0de5576a3c73da9f624a00c5a79100e860
ryudaibaba <1844ryudai@gmail.com>
2021-11-23 20:20:40 +0900

    AtCoder Beginner Contest 218 Weather Forecast コード修正

--- november/november12.php
+++ november/november12.php
@@ -6,8 +6,4 @@ $s = trim(fgets(STDIN));
 // 文字列特定
 $wether = substr($s, (int)$n - 1, 1);
 
-if ($wether === 'o') {
-    echo 'Yes';
-} else {
-    echo 'No';
-}
+echo ($wether === 'o') ? 'Yes' : 'No';
