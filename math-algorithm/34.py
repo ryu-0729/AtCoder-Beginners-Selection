7f1e1d351af0dd4bc4884a865d0ee76d9837104d
1844ryudai@gmail.com <1844ryudai>
2022-08-06 01:29:45 +0900

    fix: Add a comment

--- math-algorithm/34.py
+++ math-algorithm/34.py
@@ -1,5 +1,4 @@
 # 071 - Linear Programming
-# TODO: 線形計画法の流れは理解できたがコードの理解は全くできていない。。
 n = int(input())
 a, b, c = [0.0] * n, [0.0] * n, [0.0] * n
 
@@ -9,10 +8,13 @@ for i in range(n):
 answer = 0.0
 for i in range(n):
   for j in range(i + 1, n):
+    # 交差しない条件
     if a[i] * b[j] == a[j] * b[i]: continue
 
+    # i番目とj番目の直線の交点を求める
     px = (c[i] * b[j] - c[j] * b[i]) / (a[i] * b[j] - a[j] * b[i])
     py = (c[i] * a[j] - c[j] * a[i]) / (b[i] * a[j] - b[j] * a[i])
+
     result = True
     for k in range(n):
       if a[k] * px + b[k] * py > c[k]:
