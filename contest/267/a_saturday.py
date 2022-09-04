fd9b64ce5a9d553a27cb689a804c61f0aae54e09
1844ryudai@gmail.com <1844ryudai>
2022-09-04 11:34:40 +0900

    feat: AtCoder Contest 267

---
+++ contest/267/a_saturday.py
@@ -0,0 +1,13 @@
+S = str(input())
+day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+if S == day[0]:
+  print(5)
+elif S == day[1]:
+  print(4)
+elif S == day[2]:
+  print(3)
+elif S == day[3]:
+  print(2)
+elif S == day[4]:
+  print(1)
