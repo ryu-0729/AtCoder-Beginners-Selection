351f354a1109a746ae3be9f758780175e321cfb3
1844ryudai@gmail.com <1844ryudai>
2022-09-17 23:24:27 +0900

    feat: UNICORNプログラミングコンテスト2022(AtCoder Beginner Contest 269)

---
+++ contest/269/a_anyway-takahashi.py
@@ -0,0 +1,5 @@
+# A - Anyway Takahashi
+# https://atcoder.jp/contests/abc269/tasks/abc269_a
+a, b, c, d = map(int, input().split())
+print((a + b) * (c - d))
+print('Takahashi')
---
+++ contest/269/c_submask.py
@@ -0,0 +1,3 @@
+# C - Submask
+# https://atcoder.jp/contests/abc269/tasks/abc269_c
+# TODO: bit全探索を理解出来次第リベンジ
